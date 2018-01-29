19a27e6927b48f783044898e47a120ff664ef255
Kary <karina@arteko.mx>
2018-01-29 10:54:36 -0600

    add connect and disconnect even t handlers

--- server/server.js
+++ server/server.js
@@ -1,16 +1,30 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public')
 // for heroku
 const port = process.env.PORT || 3000
 const app = express()
+const server = http.createServer(app)
+// web socket server
+const io = socketIO(server)
 
 // create a server app
 app.use(express.static(publicPath))
 
-app.listen(port, () => {
-	console.log('Server is up on port 3000')
+// call a method on io to listen for an specific event
+io.on('connection', (socket) => {
+	console.log('New user connected')
+
+	socket.on('disconnect', () => {
+		console.log('Disconnected from server')
+	})
+})
+
+server.listen(port, () => {
+	console.log(`Server is up on port ${port}`)
 })
 
 
