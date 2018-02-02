0e5c58f504ac28f7b68cc79712531306ab8c9233
Kary <karina@arteko.mx>
2018-02-02 11:06:00 -0600

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -10,6 +10,22 @@ socket.on('disconnect', function () {
 	console.log('Disconnected from server')
 })
 
+function scrollToBottom () {
+	// selectors
+	var messages = jQuery('#messages')
+	var newMessage =  messages.children('li:last-child')
+
+	var clientHeight = messages.prop('clientHeight')
+	var scrollTop = messages.prop('scrollTop')
+	var scrollHeight = messages.prop('scrollHeight')
+	var newMessageHeight = newMessage.innerHeight()
+	var lastMessageHeight = newMessage.prev().innerHeight()
+
+	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight)
+	}
+}
+
 //custom event
 socket.on('newMessage', function (message) {
 	var formattedTime = moment(message.createdAt).format('h:mm a')
@@ -21,6 +37,7 @@ socket.on('newMessage', function (message) {
 	})
 
 	jQuery('#messages').append(html)
+	scrollToBottom()
 })
 
 socket.once('newLocationMessage', function (message) {
@@ -33,6 +50,7 @@ socket.once('newLocationMessage', function (message) {
 	})
 
 	jQuery('#messages').append(html)
+	scrollToBottom()
 })
 
 socket.emit('createMessage', {
